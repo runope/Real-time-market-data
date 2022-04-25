31ec2a27ec1e9f30ff861fd91568c48a352c3881
runope <275358499@qq.com>
2022-04-25 11:11:43 +0800

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+pub mod realtime_data;
 pub mod sina;
 pub mod tencent;
-pub mod realtime_data;
+
 use async_trait::async_trait;
 use realtime_data::ItemData;
-
 
 #[async_trait]
 pub trait GainRTData {
@@ -11,12 +11,12 @@ pub trait GainRTData {
 }
 
 // pub struct RtData {}
+#[derive(Clone, Copy, Debug)]
 pub enum RtData {
     Tencent,
 }
 
 impl RtData {
-
     pub fn init(&self) -> Box<dyn GainRTData> {
         match self {
             RtData::Tencent => Box::new(tencent::Tencent::new()),
@@ -30,10 +30,14 @@ mod tests {
 
     #[actix_web::test]
     async fn it_works() {
-
+        println!("start");
         let source = RtData::Tencent.init();
-        println!("{:#?}", source.stocks(vec!["sz000001", "sz000002"], true).await);
 
-        
+        println!(
+            "{:#?}",
+            source
+                .stocks(vec!["sz002271", "sh600036", "sz002791", "sz159928"], true)
+                .await
+        );
     }
 }
--- src/tencent.rs
+++ src/tencent.rs
@@ -1,22 +1,24 @@
-use reqwest::{header::HeaderMap, Response};
-use anyhow::{Result};
-use nom::{self, bytes::complete::{tag, take_while, is_not}, IResult};
-use chrono::naive::NaiveDateTime;
-use log::{warn, info};
+use anyhow::Result;
 use async_trait::async_trait;
+use chrono::naive::NaiveDateTime;
+use log::{info, warn};
+use nom::{
+    self,
+    bytes::complete::{is_not, tag, take_while},
+    IResult,
+};
+use reqwest::{header::HeaderMap, Response};
 
-use crate::realtime_data::ItemData;
-use crate::GainRTData;
+use crate::{realtime_data::ItemData, GainRTData};
 
 pub struct Tencent {
-    headers: reqwest::header::HeaderMap,
+    headers:   reqwest::header::HeaderMap,
     stock_api: &'static str,
 }
 
 #[async_trait]
 impl GainRTData for Tencent {
     async fn stocks(&self, stocks_list: Vec<&str>, prefix: bool) -> Vec<ItemData> {
-
         let resp = self.get_stocks(stocks_list).await.unwrap();
 
         self.format_response_data(resp).await.unwrap()
@@ -42,7 +44,10 @@ impl Tencent {
         let mut headers = HeaderMap::new();
         headers.insert(
             "User-Agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36".parse().unwrap()
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+             Chrome/100.0.4896.88 Safari/537.36"
+                .parse()
+                .unwrap(),
         );
         headers.insert("Referer", "https://gu.qq.com/".parse().unwrap());
         // headers.insert("Accept-Encoding", "gzip, deflate, br".parse().unwrap());
@@ -54,22 +59,17 @@ impl Tencent {
         }
     }
 
-
-
     pub async fn get_stocks(&self, stocks_list: Vec<&str>) -> Result<Response> {
         let mut params = String::from("q=");
         for i in stocks_list {
             params = params + i + ",";
         }
-        
+
         let mut url = format!("{}?{}", self.stock_api, params);
         url.pop();
 
         let client = reqwest::Client::new();
-        let resp = client.get(url)
-            .headers(self.headers.clone())
-            .send()
-            .await?;
+        let resp = client.get(url).headers(self.headers.clone()).send().await?;
 
         Ok(resp)
     }
@@ -77,8 +77,8 @@ impl Tencent {
     pub fn parse(input: &str) -> IResult<&str, ItemData> {
         let mut rt_data = ItemData::default();
         let (input, _) = tag("v_")(input)?;
-        
-        let (input, resp) = take_while( |c| c != '=')(input)?;
+
+        let (input, resp) = take_while(|c| c != '=')(input)?;
         rt_data.code = resp.to_string();
         let (input, _) = is_not("~")(input)?;
         let resp = input.split('~').collect::<Vec<&str>>();
@@ -113,8 +113,11 @@ impl Tencent {
         match NaiveDateTime::parse_from_str(resp[30], "%Y%m%d%H%M%S") {
             Ok(dt) => rt_data.datatime = Some(dt),
             Err(e) => {
-                rt_data.datatime = None; 
-                warn!("parse datatime error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
+                rt_data.datatime = None;
+                warn!(
+                    "parse datatime error: {}. code: {}, name: {}",
+                    e, rt_data.code, rt_data.name
+                );
             }
         };
 
@@ -122,13 +125,16 @@ impl Tencent {
         rt_data.gain_percentage = resp[32].parse::<f32>().unwrap();
         rt_data.high = resp[33].parse::<f32>().unwrap();
         rt_data.low = resp[34].parse::<f32>().unwrap();
-        rt_data.total_value = resp[37].parse::<f64>().unwrap()*10000.0;
+        rt_data.total_value = resp[37].parse::<f64>().unwrap() * 10000.0;
         rt_data.turnover = match resp[38].parse::<f32>() {
             Ok(v) => Some(v),
             Err(e) => {
-                warn!("parse turnover error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
+                warn!(
+                    "parse turnover error: {}. code: {}, name: {}",
+                    e, rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
 
         rt_data.pe = match resp[39].parse::<f32>() {
@@ -138,11 +144,14 @@ impl Tencent {
                 } else {
                     Some(v)
                 }
-            },
+            }
             Err(e) => {
-                warn!("parse pe error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
+                warn!(
+                    "parse pe error: {}. code: {}, name: {}",
+                    e, rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
 
         rt_data.pb = match resp[46].parse::<f32>() {
@@ -152,11 +161,14 @@ impl Tencent {
                 } else {
                     Some(v)
                 }
-            },
+            }
             Err(e) => {
-                warn!("parse pb error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
+                warn!(
+                    "parse pb error: {}. code: {}, name: {}",
+                    e, rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
 
         rt_data.amplitude = resp[43].parse::<f32>().unwrap();
@@ -167,11 +179,14 @@ impl Tencent {
                 } else {
                     Some(v)
                 }
-            },
+            }
             Err(e) => {
-                warn!("parse traded_market_value error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
+                warn!(
+                    "parse traded_market_value error: {}. code: {}, name: {}",
+                    e, rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
 
         rt_data.market_value = match resp[45].parse::<f32>() {
@@ -181,11 +196,14 @@ impl Tencent {
                 } else {
                     Some(v)
                 }
-            },
+            }
             Err(e) => {
-                warn!("parse market_value error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
+                warn!(
+                    "parse market_value error: {}. code: {}, name: {}",
+                    e, rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
 
         rt_data.high_limit = resp[47].parse::<f32>().unwrap();
@@ -193,47 +211,58 @@ impl Tencent {
         rt_data.quantity_relative_ratio = match resp[49].parse::<f32>() {
             Ok(v) => Some(v),
             Err(e) => {
-                warn!("parse quantity_relative_ratio error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
+                warn!(
+                    "parse quantity_relative_ratio error: {}. code: {}, name: {}",
+                    e, rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
-        
+
         rt_data.entrust_different = match resp.get(50) {
-            Some(&v) => {
-                match v.parse::<f32>() {
-                    Ok(v) => Some(v),
-                    Err(e) => {
-                        warn!("parse entrust_different error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
-                        None
-                    }
+            Some(&v) => match v.parse::<f32>() {
+                Ok(v) => Some(v),
+                Err(e) => {
+                    warn!(
+                        "parse entrust_different error: {}. code: {}, name: {}",
+                        e, rt_data.code, rt_data.name
+                    );
+                    None
                 }
             },
             None => {
-                info!("no entrust_different in data. code: {}, name: {}", rt_data.code, rt_data.name);
+                info!(
+                    "no entrust_different in data. code: {}, name: {}",
+                    rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
-    
+
         rt_data.average_price = match resp.get(51) {
-            Some(&v) => {
-                match v.parse::<f32>() {
-                    Ok(v) => Some(v),
-                    Err(e) => {
-                        warn!("parse entrust_different error: {}. code: {}, name: {}", e, rt_data.code, rt_data.name);
-                        None
-                    }
+            Some(&v) => match v.parse::<f32>() {
+                Ok(v) => Some(v),
+                Err(e) => {
+                    warn!(
+                        "parse entrust_different error: {}. code: {}, name: {}",
+                        e, rt_data.code, rt_data.name
+                    );
+                    None
                 }
             },
             None => {
-                info!("no average_price in data. code: {}, name: {}", rt_data.code, rt_data.name);
+                info!(
+                    "no average_price in data. code: {}, name: {}",
+                    rt_data.code, rt_data.name
+                );
                 None
-            },
+            }
         };
 
         Ok(("", rt_data))
     }
 
-    pub async fn format_response_data(&self, response: reqwest::Response) -> Result<Vec<ItemData>>{
+    pub async fn format_response_data(&self, response: reqwest::Response) -> Result<Vec<ItemData>> {
         let data = response.text().await?;
         let data_details = data.split(";");
 
@@ -243,12 +272,12 @@ impl Tencent {
             match Self::parse(i.trim()) {
                 Ok((_, rt_data)) => {
                     res.push(rt_data);
-                },
+                }
                 Err(e) => {
                     warn!("parse error: {}", e);
                 }
             };
-        };
+        }
 
         Ok(res)
     }
@@ -270,9 +299,21 @@ mod tests {
 
     #[test]
     fn test_parse_data() {
-        
-        let parse1 = "v_sz000001=\"51~平安银行~000001~15.81~15.90~15.90~821772~381881~439892~15.81~506~15.80~1439~15.79~2145~15.78~3932~15.77~687~15.82~343~15.83~2665~15.84~1449~15.85~2681~15.86~1157~~20220419161403~-0.09~-0.57~15.97~15.62~15.81/821772/1294226951~821772~129423~0.42~8.44~~15.97~15.62~2.20~3068.01~3068.08~0.94~17.49~14.31~0.79~414~15.75~8.44~8.44~~~1.33~129422.6951~0.0000~0~ ~GP-A~-4.07~-0.69~1.14~9.19~0.74~25.16~13.22~0.38~7.55~-3.18~19405522500~19405918750~2.43~-23.25~19405522500~\"";
-        let parse2 = "v_sz000002=\"51~万  科Ａ~000002~20.66~20.53~20.54~1199798~620368~579430~20.66~2554~20.65~474~20.64~194~20.63~476~20.62~75~20.67~333~20.68~1136~20.69~668~20.70~6720~20.71~504~~20220419161403~0.13~0.63~20.70~19.67~20.66/1199798/2423548170~1199798~242355~1.23~10.66~~20.70~19.67~5.02~2007.65~2401.80~1.02~22.58~18.48~0.78~-5588~20.20~10.66~10.66~~~1.28~242354.8170~0.0000~0~ ~GP-A~4.55~2.43~6.05~9.55~1.96~27.83~14.43~0.00~18.40~-0.19~9717553125~11625383750~-42.55~-5.40~9717553125~\"";
+        let parse1 = "v_sz000001=\"51~平安银行~000001~15.81~15.90~15.90~821772~381881~439892~15.\
+                      81~506~15.80~1439~15.79~2145~15.78~3932~15.77~687~15.82~343~15.83~2665~15.\
+                      84~1449~15.85~2681~15.86~1157~~20220419161403~-0.09~-0.57~15.97~15.62~15.81/\
+                      821772/1294226951~821772~129423~0.42~8.44~~15.97~15.62~2.20~3068.01~3068.\
+                      08~0.94~17.49~14.31~0.79~414~15.75~8.44~8.44~~~1.33~129422.6951~0.0000~0~ \
+                      ~GP-A~-4.07~-0.69~1.14~9.19~0.74~25.16~13.22~0.38~7.55~-3.\
+                      18~19405522500~19405918750~2.43~-23.25~19405522500~\"";
+        let parse2 = "v_sz000002=\"51~万  \
+                      科Ａ~000002~20.66~20.53~20.54~1199798~620368~579430~20.66~2554~20.65~474~20.\
+                      64~194~20.63~476~20.62~75~20.67~333~20.68~1136~20.69~668~20.70~6720~20.\
+                      71~504~~20220419161403~0.13~0.63~20.70~19.67~20.66/1199798/\
+                      2423548170~1199798~242355~1.23~10.66~~20.70~19.67~5.02~2007.65~2401.80~1.\
+                      02~22.58~18.48~0.78~-5588~20.20~10.66~10.66~~~1.28~242354.8170~0.0000~0~ \
+                      ~GP-A~4.55~2.43~6.05~9.55~1.96~27.83~14.43~0.00~18.40~-0.\
+                      19~9717553125~11625383750~-42.55~-5.40~9717553125~\"";
 
         match Tencent::parse(parse1) {
             Ok((_, rt_data)) => {
@@ -304,7 +345,10 @@ mod tests {
                 assert_eq!(rt_data.ask4_volume, 268100);
                 assert_eq!(rt_data.ask5, 15.86);
                 assert_eq!(rt_data.ask5_volume, 115700);
-                assert_eq!(rt_data.datatime, Some(NaiveDateTime::parse_from_str("20220419161403", "%Y%m%d%H%M%S").unwrap()));
+                assert_eq!(
+                    rt_data.datatime,
+                    Some(NaiveDateTime::parse_from_str("20220419161403", "%Y%m%d%H%M%S").unwrap())
+                );
                 assert_eq!(rt_data.gain_amout, -0.09);
                 assert_eq!(rt_data.gain_percentage, -0.57);
                 assert_eq!(rt_data.high, 15.97);
@@ -321,10 +365,10 @@ mod tests {
                 assert_eq!(rt_data.quantity_relative_ratio, Some(0.79));
                 assert_eq!(rt_data.entrust_different, Some(414.0));
                 assert_eq!(rt_data.average_price, Some(15.75));
-            },
+            }
             Err(e) => {
                 panic!("parse error: {}", e);
-            },
+            }
         }
 
         match Tencent::parse(parse2) {
@@ -357,7 +401,10 @@ mod tests {
                 assert_eq!(rt_data.ask4_volume, 672000);
                 assert_eq!(rt_data.ask5, 20.71);
                 assert_eq!(rt_data.ask5_volume, 50400);
-                assert_eq!(rt_data.datatime, Some(NaiveDateTime::parse_from_str("20220419161403", "%Y%m%d%H%M%S").unwrap()));
+                assert_eq!(
+                    rt_data.datatime,
+                    Some(NaiveDateTime::parse_from_str("20220419161403", "%Y%m%d%H%M%S").unwrap())
+                );
                 assert_eq!(rt_data.gain_amout, 0.13);
                 assert_eq!(rt_data.gain_percentage, 0.63);
                 assert_eq!(rt_data.high, 20.7);
@@ -374,16 +421,19 @@ mod tests {
                 assert_eq!(rt_data.quantity_relative_ratio, Some(0.78));
                 assert_eq!(rt_data.entrust_different, Some(-5588.0));
                 assert_eq!(rt_data.average_price, Some(20.2));
-            },
+            }
             Err(e) => {
                 panic!("parse error: {}", e);
-            },
+            }
         }
     }
 
     #[test]
     fn test_parse_data2() {
-        let data = "v_sh518801=\"1~国泰申赎~518801~2.229~2.229~0.000~0~0~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~~20151224150221~0.000~0.00~0.000~0.000~2.230/0/0~0~0~~~~0.000~0.000~0.00~~~0.000~2.452~2.006~\"";
+        let data = "v_sh518801=\"1~国泰申赎~518801~2.229~2.229~0.000~0~0~0~0.000~0~0.000~0~0.\
+                    000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.000~0~0.\
+                    000~0~~20151224150221~0.000~0.00~0.000~0.000~2.230/0/0~0~0~~~~0.000~0.000~0.\
+                    00~~~0.000~2.452~2.006~\"";
         match Tencent::parse(data) {
             Ok((_, rt_data)) => {
                 assert_eq!(rt_data.code, "sh518801");
@@ -414,7 +464,10 @@ mod tests {
                 assert_eq!(rt_data.ask4_volume, 0);
                 assert_eq!(rt_data.ask5, 0.0);
                 assert_eq!(rt_data.ask5_volume, 0);
-                assert_eq!(rt_data.datatime, Some(NaiveDateTime::parse_from_str("20151224150221", "%Y%m%d%H%M%S").unwrap()));
+                assert_eq!(
+                    rt_data.datatime,
+                    Some(NaiveDateTime::parse_from_str("20151224150221", "%Y%m%d%H%M%S").unwrap())
+                );
                 assert_eq!(rt_data.gain_amout, 0.000);
                 assert_eq!(rt_data.gain_percentage, 0.000);
                 assert_eq!(rt_data.high, 0.000);
@@ -431,11 +484,10 @@ mod tests {
                 assert_eq!(rt_data.quantity_relative_ratio, None);
                 assert_eq!(rt_data.entrust_different, None);
                 assert_eq!(rt_data.average_price, None);
-            },
+            }
             Err(e) => {
                 panic!("parse error: {}", e);
-            },
+            }
         }
     }
 }
-
